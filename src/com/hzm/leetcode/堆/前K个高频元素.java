156d8fc5b97b07cf4a74b67b46525ba3c03e0afc
hzmpay <hzmpay@163.com>
2020-12-23 14:47:12 +0800

    LeetCode部分题解答

--- src/com/hzm/leetcode/堆/前K个高频元素.java
+++ src/com/hzm/leetcode/堆/前K个高频元素.java
@@ -1,9 +1,7 @@
 package com.hzm.leetcode.堆;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * https://leetcode-cn.com/problems/top-k-frequent-elements/
@@ -15,23 +13,71 @@ import java.util.stream.Collectors;
 public class 前K个高频元素 {
 
     public static void main(String[] args) {
-        int[] nums = {5, 6, 8, 7, 10, 1};
-        sort(nums, 0, nums.length - 1);
-        System.out.println(Arrays.toString(nums));
+        int[] nums = {1,1,1,2,2,33333333};
+//        sort(nums, 0, nums.length - 1);
+        int[] ints = topKFrequent2(nums, 2);
+        System.out.println(Arrays.toString(ints));
     }
 
-    public static int[] topKFrequent(int[] nums, int k) {
+    public static int[] topKFrequent2(int[] nums, int k) {
+        // 构建出现次数Map<值，出现次数>
+        Map<Integer, Integer> map = new HashMap<>(nums.length / 3 * 4);
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // 按照value值从小到大排
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue((Comparator<Map.Entry<Integer, Integer>>) (o1, o2) -> o1.getValue() - o2.getValue());
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            // 存入K个数即可
+            if (queue.size() == k) {
+                // 判断是否比栈顶的值大
+                if (entry.getValue() > queue.peek().getValue()) {
+                    // 出栈
+                    queue.poll();
+                    // 入栈
+                    queue.offer(entry);
+                }
+            } else {
+                queue.offer(entry);
+            }
+        }
+        int[] result = new int[k];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = queue.poll().getKey();
+        }
+        return result;
+    }
+
+    /**
+     * 快排方式
+     *
+     * @param nums
+     * @param k
+     * @return int[]
+     * @author Hezeming
+     */
+    public static int[] topKFrequent1(int[] nums, int k) {
         // 构建出现次数Map<值，出现次数>
         Map<Integer, Integer> map = new HashMap<>(nums.length / 3 * 4);
         for (int num : nums) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
         map.keySet();
+        Map.Entry[] entryArr = map.entrySet().toArray(new Map.Entry[0]);
+        sort(entryArr, 0, entryArr.length - 1);
 
-        return nums;
+
+        int[] result = new int[k];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = (int) entryArr[i].getKey();
+        }
+
+        return result;
     }
 
-    public static void sort(int[] nums, int l, int r) {
+    public static void sort(Map.Entry[] nums, int l, int r) {
         if (l >= r) {
             return;
         }
@@ -39,25 +85,25 @@ public class 前K个高频元素 {
         int right = r;
 
         // 最左边的当做中间值
-        int diffNum = left;
+        int temp = (int) nums[l].getValue();
         // 直到左右指针相遇
         while (left < right) {
-            while (nums[left] <= nums[diffNum] && left < right) {
-                left++;
-            }
-            swap(nums, left, diffNum);
-
-            while (nums[right] >= nums[diffNum] && left < right) {
+            while (((int) nums[right].getValue() <= temp) && (left < right)) {
                 right--;
             }
-            swap(nums, right, diffNum);
+            swap(nums, right, left);
+
+            while (((int) nums[left].getValue() >= temp) && (left < right)) {
+                left++;
+            }
+            swap(nums, left, right);
         }
         sort(nums, l, left);
         sort(nums, left + 1, r);
     }
 
-    public static void swap(int[] nums, int l, int r) {
-        int temp = nums[l];
+    public static void swap(Map.Entry[] nums, int l, int r) {
+        Map.Entry temp = nums[l];
         nums[l] = nums[r];
         nums[r] = temp;
     }
